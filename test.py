56ec0dc89ab1057f1d4a846c75557d54bf518540
Shubham Kandpal <62210890+Shubham111Kandpal@users.noreply.github.com>
2023-11-08 09:27:50 +0000

    Create test.py
    importing python packages

---
+++ test.py
@@ -0,0 +1,16 @@
+
+import array
+import random
+import json
+
+import numpy
+
+from math import sqrt
+
+from deap import algorithms
+from deap import base
+from deap import benchmarks
+from deap.benchmarks.tools import diversity, convergence, hypervolume
+from deap import creator
+from deap import tools
+import matplotlib.pyplot as plt
